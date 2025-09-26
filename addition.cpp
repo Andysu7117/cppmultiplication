e3259036abd92791f8d1bfd8e5ebe8431e865670
Andy <andy.su7117@gmail.com>
2025-09-26 15:15:20 +1000

    scanning numbers fnction

--- addition.cpp
+++ addition.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+void scanNumbers(int *a, int *b) {
+    std::cout << "Enter an integer: ";
+    std::cin >> *a;
+
+    std::cout << "Enter another integer: ";
+    std::cin >> *b;
+}
+
 int addition(int a, int b) {
     return a + b;
 }
@@ -9,12 +17,10 @@ int subtract(int a, int b) {
 }
 
 int main() {
-    std::cout << "Enter an integer: ";
     int num1{ };
-    std::cin >> num1;
-    std::cout << "Enter another integer: ";
     int num2{ };
-    std::cin >> num2;
+    scanNumbers(&num1, &num2);
+
     std::cout << num1 << " + " << num2 << " is " << addition(num1, num2) << ".\n";
     std::cout << num1 << " - " << num2 << " is " << subtract(num1, num2) << ".\n";
     return 0;
