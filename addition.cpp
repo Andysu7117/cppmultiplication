afff78df66f0141c951a010a19d97446510ad85e
Andy <andy.su7117@gmail.com>
2025-09-26 15:10:11 +1000

    addition and subtraction functions

--- addition.cpp
+++ addition.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+int addition(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
 int main() {
     std::cout << "Enter an integer: ";
     int num1{ };
@@ -7,7 +15,7 @@ int main() {
     std::cout << "Enter another integer: ";
     int num2{ };
     std::cin >> num2;
-    std::cout << num1 << " + " << num2 << " is " << num1 + num2 << ".\n";
-    std::cout << num1 << " - " << num2 << " is " << num1 - num2 << ".\n";
+    std::cout << num1 << " + " << num2 << " is " << addition(num1, num2) << ".\n";
+    std::cout << num1 << " - " << num2 << " is " << subtract(num1, num2) << ".\n";
     return 0;
 }
