e5a28c01868b4a185cb8bbc51357768374eb71ce
Andy <andy.su7117@gmail.com>
2025-09-26 14:26:56 +1000

    compiled

--- multiplication.cpp
+++ multiplication.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
-int main(void) {
+int main() {
     std::cout << "Enter an integer: ";
-    int x {};
+    int x{ };
     std::cin >> x;
 
-    std::cout << "Double " << x << "is: " << x * 2 << '\n';
-    std::cout << "Triple " << x << "is: " << x * 3 << '\n';
+    std::cout << "Double " << x << " is: " << x * 2 << '\n';
+    std::cout << "Triple " << x << " is: " << x * 3 << '\n';
     return 0;
 }
