c4b8fc07fb57f17d01a669389acfebe760fa5231
Andy <andy.su7117@gmail.com>
2025-09-26 14:18:55 +1000

    Simple scan in number and multiply

---
+++ multiplication.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(void) {
+    std::cout << "Enter an integer: ";
+    int x {};
+    std::cin >> x;
+
+    std::cout << "Double " << x << "is: " << x * 2 << '\n';
+    std::cout << "Triple " << x << "is: " << x * 3 << '\n';
+    return 0;
+}
